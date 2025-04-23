e30560b7d23c100842b77fc66f95a2590879e000
Angela Yu <angela.yu@emory.edu>
2025-04-23 17:01:34 -0400

    Update

--- code/02_render_report.R
+++ code/02_render_report.R
@@ -2,4 +2,4 @@ here::i_am("code/02_render_report.R")
 
 library(rmarkdown)
 # rendering a report in production mode
-render("midterm_report.Rmd")
+render("midterm_report.Rmd", output_file = "Midterm_Project_Report.html")
